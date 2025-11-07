d718ddefc72ee5db41420eb36d296d56c4b11d98
Dani <dpalmel1312@ieszaidinvergeles.org>
2025-11-07 14:31:40 +0100

    E_Condicionales_10

---
+++ Ejercicios_Condicionales/Ejercicio_10.py
@@ -0,0 +1,7 @@
+pizza=input("Quieres una pizza Vegetariana o no Vegetariana?: ")
+if(pizza=="Vegetariana"):
+    ingrediente=input("Dime que ingrdiente quieres pimiento o tofu: ")
+else:
+    ingrediente=input("Dime que ingrdiente quieres Peperoni ,Jamon o Salmon: ")
+
+print("La pizza es ",pizza,"y sus ingredientes son",ingrediente)
