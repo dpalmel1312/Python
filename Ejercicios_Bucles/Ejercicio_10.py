bf9e44ab5b15b7617184b9336874edc23dca6a99
Dani <dpalmel1312@ieszaidinvergeles.org>
2026-01-08 09:54:45 +0100

    E_Bucle_12

--- Ejercicios_Bucles/Ejercicio_10.py
+++ Ejercicios_Bucles/Ejercicio_10.py
@@ -2,7 +2,7 @@ num = int(input("Introduce un número entero positivo mayor que 2: "))
 i = 2
 while num % i != 0:
     i = i+1
-if i == n:
+if i == num:
     print(str(num) + " es primo")
 else:
     print(str(num) + " no es primo")
